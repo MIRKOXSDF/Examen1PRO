29b841d037436cb9db5611341ef06bb66af575ce
MIRKOXSDF <mirko06660o@gmail.com>
2024-09-20 16:52:03 -0500

    Contenido de semilla

--- Exm1PRO/Semilla.cs
+++ Exm1PRO/Semilla.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exm1PRO
 {
-    class Semilla
+    abstract class Semilla
     {
+        public string nombre;
+        public int tiempoDeCrecimiento;
+        protected Semilla(string nombre, int tiempoDeCrecimiento)
+        {
+            this.nombre = nombre;
+            this.tiempoDeCrecimiento = tiempoDeCrecimiento;
+        }
+        public abstract void Plantar(bool plantastes);
+        
+        
+
+        
+
     }
 }
