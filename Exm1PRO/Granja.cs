1d265e2553c05666fef938b2c0d5945b4bea0e28
MIRKOXSDF <mirko06660o@gmail.com>
2024-09-20 17:37:44 -0500

    Avance

---
+++ Exm1PRO/Granja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exm1PRO
+{
+    class Granja
+    {
+        List<Animals> animal;
+
+        public void Game()
+        {
+
+        }
+    }
+}
