fa2498b8e33321e4d47f2213a243e3e36cb0b06a
MIRKOXSDF <mirko06660o@gmail.com>
2024-09-20 17:41:31 -0500

    Create granjero

---
+++ Exm1PRO/Granjero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exm1PRO
+{
+    class Granjero
+    {
+        public string nombre;
+        public float dinero;
+
+        public Granjero(string nombre, float dinero)
+        {
+            this.nombre = nombre;
+            this.dinero = dinero;
+        }
+    }
+}
