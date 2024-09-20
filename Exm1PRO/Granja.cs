d457797f12088b15db56e1da6c2e7fa89a6d3e78
MIRKOXSDF <mirko06660o@gmail.com>
2024-09-20 18:13:51 -0500

    Avance

--- Exm1PRO/Granja.cs
+++ Exm1PRO/Granja.cs
@@ -9,9 +9,29 @@ namespace Exm1PRO
     class Granja
     {
         List<Animals> animal;
+        Granjero granjero;
+        float dinero;
 
         public void Game()
         {
+            Console.WriteLine("Coloca el nombre del granjero : ");
+            string nombre = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Coloca la cantidad de dinero : ");
+                dinero = float.Parse(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Entrada no válida. Por favor, ingresa un número entero.");
+               
+            }
+
+
+
+            granjero = new Granjero(nombre, dinero);
+            Console.WriteLine($"El granjero se llama {nombre} y tiene  {dinero} de dinero");
+
 
         }
     }
